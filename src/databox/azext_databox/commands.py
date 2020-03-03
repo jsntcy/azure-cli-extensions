e8e08c572c931aee3303ea2400d9e7ef27dc463f
Yu Chen <ychenu@microsoft.com>
2020-03-03 17:42:45 +0800

    test azdev linter databox

--- src/databox/azext_databox/commands.py
+++ src/databox/azext_databox/commands.py
@@ -7,17 +7,19 @@
 # pylint: disable=too-many-lines
 # pylint: disable=too-many-statements
 # pylint: disable=too-many-locals
+from azext_databox._validators import validate_destination_account_details
 from azure.cli.core.commands import CliCommandType
 
 
 def load_command_table(self, _):
 
-    from ._client_factory import cf_jobs
+    from azext_databox._client_factory import cf_jobs
     databox_jobs = CliCommandType(
         operations_tmpl='azext_databox.vendored_sdks.databox.operations._jobs_operations#JobsOperations.{}',
         client_factory=cf_jobs)
     with self.command_group('databox job', databox_jobs, client_factory=cf_jobs) as g:
-        g.custom_command('create', 'create_databox_job')
+        g.custom_command('create', 'create_databox_job', validator=validate_destination_account_details)
+        g.custom_command('update', 'update_databox_job')
         g.custom_command('delete', 'delete_databox_job', confirmation=True)
         g.custom_show_command('show', 'get_databox_job')
         g.custom_command('list', 'list_databox_job')
